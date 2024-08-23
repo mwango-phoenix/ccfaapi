c913deca1d44431dff8f94cd2a5fc23a846fd8de
Mick <67757350+mwango-phoenix@users.noreply.github.com>
2024-08-23 21:30:06 +0000

    handle CORS requests from https://theccfa.net

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import getVolunteerOpportunities from '../api/getVolunteerOpportunities.mjs';
 import registerForEvent from '../api/registerVolunteer.mjs';
 import getUserData from '../api/getUserData.mjs';
@@ -6,11 +7,16 @@ import getUserData from '../api/getUserData.mjs';
 const app = express();
 const port = 3000;
 
+// Use the cors middleware with specific origin
+app.use(cors({
+    origin: 'https://theccfa.net',
+}));
+
 app.use(express.json());
 
 app.get('/getVolunteerOpportunities', getVolunteerOpportunities);
-app.post('/registerVolunteer', registerForEvent)
-app.get('/getUserData', getUserData)
+app.post('/registerVolunteer', registerForEvent);
+app.get('/getUserData', getUserData);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
