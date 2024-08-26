6b79f1eab5850885a9efb84aee76beb5a30ef28e
Mick <67757350+mwango-phoenix@users.noreply.github.com>
2024-08-26 16:49:44 +0000

    fix api handler

--- api/handler.mjs
+++ api/handler.mjs
@@ -1,16 +1,26 @@
-import { getEventDetails } from './getVolunteerOpportunities.mjs';
-import registerForEvent  from './registerVolunteer.mjs';
+import getVolunteerOpportunities from './getVolunteerOpportunities.mjs';
+import registerForEvent from './registerVolunteer.mjs';
+import getUserData from './getUserData.mjs';
 
 export default async function handler(req, res) {
     try {
-        if (req.method === 'POST') {
+        // Add CORS headers
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        // Determine the route and handle accordingly
+        if (req.url === '/getVolunteerOpportunities' && req.method === 'GET') {
+            console.log("Getting Opportunities List")
+            await getVolunteerOpportunities(req, res);
+        } else if (req.url === '/getUserData' && req.method === 'GET') {
+            await getUserData(req, res);
+        } else if (req.url === '/registerVolunteer' && req.method === 'POST') {
             const { eventId, userData } = req.body;
             const registrationResult = await registerForEvent(eventId, userData);
             res.status(200).json(registrationResult);
         } else {
-            const events = await getEventDetails();
-            console.log("yay")
-            res.status(200).json(events);
+            res.status(404).json({ error: 'Route not found' });
         }
     } catch (error) {
         console.error('Error handling request:', error);
