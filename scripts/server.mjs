4c8f6b50233147e5d100e05063a1d483691cbfef
Mick <67757350+mwango-phoenix@users.noreply.github.com>
2024-08-27 18:19:50 +0000

    test

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import getVolunteerOpportunities from '../api/getVolunteerOpportunities.mjs';
-import registerForEvent from '../api/registerVolunteer.mjs';
-import getUserData from '../api/getUserData.mjs';
+// import express from 'express';
+// import cors from 'cors';
+// import getVolunteerOpportunities from '../api/getVolunteerOpportunities.mjs';
+// import registerForEvent from '../api/registerVolunteer.mjs';
+// import getUserData from '../api/getUserData.mjs';
 
-const app = express();
-const port = 3000;
-const corsOptions = {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-};
-app.use(cors(corsOptions));
-app.use(express.json());
+// const app = express();
+// const port = 3000;
+// const corsOptions = {
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true,
+// };
+// app.use(cors(corsOptions));
+// app.use(express.json());
 
-app.get('/getVolunteerOpportunities', getVolunteerOpportunities);
-app.post('/registerVolunteer', registerForEvent);
-app.get('/getUserData',getUserData);
+// app.get('/getVolunteerOpportunities', getVolunteerOpportunities);
+// app.post('/registerVolunteer', registerForEvent);
+// app.get('/getUserData',getUserData);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
