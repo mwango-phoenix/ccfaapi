c23603a79c7807f3b0780ff9f03ec7e51e44923f
Mick <67757350+mwango-phoenix@users.noreply.github.com>
2024-08-27 18:33:55 +0000

    update endpoint to register users for event

--- api/registerVolunteer.mjs
+++ api/registerVolunteer.mjs
@@ -1,54 +1,61 @@
 import { getAccessToken, postRequest } from '../utils/apiUtils.mjs';
 
-export default async function registerForEvent(eventId, userData) {
-    const accessToken = await getAccessToken();
-    const accountId = process.env.ACCOUNT_ID;
+export default async function registerForEvent(res, req) {
+    const { eventId, userData } = req.body;
     
-    const registrationData = {
-        "Id": 0,
-        "Event": {
-            "Id": eventId
-        },
-        "Contact": {
-            "Id": userData.contactId
-        },
-        "RegistrationTypeId": 9326765, 
-        "GuestRegistrationsSummary": {
-            "NumberOfGuests": 0,
-            "NumberOfGuestsCheckedIn": 0,
-            "GuestRegistrations": []
-        },
-        "IsCheckedIn": false,
-        "RegistrationFields": [
-            {
-                "FieldName": "First name",
-                "Value": userData.firstName,
-                "SystemCode": "FirstName"
-            },
-            {
-                "FieldName": "Last name",
-                "Value": userData.lastName,
-                "SystemCode": "LastName"
-            },
-            {
-                "FieldName": "e-Mail",
-                "Value": userData.email,
-                "SystemCode": "Email"
-            },
-            {
-                "FieldName": "Phone",
-                "Value": userData.phone,
-                "SystemCode": "Phone"
-            },
-            {
-                "FieldName": "Waiver",
-                "Value": true,
-                "SystemCode": "custom-14890495"
-            }
-        ],
-        "ShowToPublic": false
-    };
+    try {
+        const accessToken = await getAccessToken();
+        const accountId = process.env.ACCOUNT_ID;
 
-    const url = `https://api.wildapricot.org/v2.3/accounts/${accountId}/eventregistrations`;
-    return await postRequest(url, accessToken, registrationData);
-}
+        const registrationData = {
+            "Event": {
+                "Id": eventId
+            },
+            "Contact": {
+                "Id": userData.contactId
+            },
+            "RegistrationTypeId": 9326765,
+            "GuestRegistrationsSummary": {
+                "NumberOfGuests": 0,
+                "NumberOfGuestsCheckedIn": 0,
+                "GuestRegistrations": []
+            },
+            "IsCheckedIn": false,
+            "RegistrationFields": [
+                {
+                    "FieldName": "First name",
+                    "Value": userData.firstName,
+                    "SystemCode": "FirstName"
+                },
+                {
+                    "FieldName": "Last name",
+                    "Value": userData.lastName,
+                    "SystemCode": "LastName"
+                },
+                {
+                    "FieldName": "e-Mail",
+                    "Value": userData.email,
+                    "SystemCode": "Email"
+                },
+                {
+                    "FieldName": "Phone",
+                    "Value": userData.phone,
+                    "SystemCode": "Phone"
+                },
+                {
+                    "FieldName": "Waiver",
+                    "Value": true,
+                    "SystemCode": "custom-14890495"
+                }
+            ],
+            "ShowToPublic": false
+        };
+
+        const url = `https://api.wildapricot.org/v2.3/accounts/${accountId}/eventregistrations`;
+        const result = await postRequest(url, accessToken, registrationData);
+        res.json(result);
+    } catch (error) {
+        console.error('Error registering for event:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
